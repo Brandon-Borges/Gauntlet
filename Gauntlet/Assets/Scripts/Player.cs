cbc227709554c5722cdfa8c46b869e89a4bdc64b
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-04-16 13:21:12 -0700

    Added Projectile Materials

--- Gauntlet/Assets/Scripts/Player.cs
+++ Gauntlet/Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
     public int keyCount;
 
     public bool isFiring;
+    public GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
