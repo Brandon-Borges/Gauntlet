7ca97ad84938e3deb0984f54f7397dddae315008
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-05-11 15:01:11 -0700

    Update Player.cs

--- Gauntlet/Assets/Scripts/Player.cs
+++ Gauntlet/Assets/Scripts/Player.cs
@@ -57,9 +57,7 @@ public class Player : MonoBehaviour
             GameObject bullet = Instantiate(projectile, transform.position, projectile.transform.rotation);
             bullet.gameObject.GetComponent<Projectile>().assignShooter(this.gameObject.transform.tag);
             bullet.gameObject.GetComponent<Projectile>().forwardMovement = testForward;
-            Debug.Log("Player.shoot, isFiring is true, should be pausing");
             yield return new WaitForSeconds(fireSpeed);
-            Debug.Log("Player.shoot, isFiring is false, it should have been some time for a pause");
             //if (!isfiring) stop this function
             //if (!isFiring) yield break;
         }
